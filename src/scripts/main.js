93f021e71e51d110f49c8bde9c37051384b6feb0
Kim Hart <kimhart90@gmail.com>
2016-11-18 19:42:44 -0500

    slideshow + likes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,45 @@
+// Assign version numbers to images
+
 let images = Array.from(document.querySelectorAll('.image'));
 let d = new Date();
 
 images.forEach((image, index) => {
   image.src = `${image.src}?ver=${d.getTime()}${index}`;
 });
+
+
+// "Liking" images in the slideshow
+
+let slides = document.getElementById('image-slideshow');
+let likeButton = document.getElementById('unliked');
+let redButton = document.getElementById('liked');
+let isLiked = false;
+
+let showLikeButton = () => {
+  likeButton.classList.remove('hide');
+};
+
+let hideBothButtons = () => {
+  likeButton.classList.add('hide');
+  redButton.classList.add('hide');
+};
+
+let toggleRedButton = () => {
+  likeButton.classList.add('hide');
+  redButton.classList.remove('hide');
+};
+
+redButton.addEventListener('click', () => {
+ likeButton.classList.add('hide');
+});
+
+slides.addEventListener('mouseenter', showLikeButton);
+slides.addEventListener('mouseleave', hideBothButtons);
+likeButton.addEventListener('click', toggleRedButton);
+
+
+
+// on a hover, show the plain button
+// when not hovering, hide the plain button
+// if they click the button, the plain one needs to disappear and the red one needs to appear
+
