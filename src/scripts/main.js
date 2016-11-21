f5882f77fd3905c74971791facb69f63357d7b69
Kim Hart <kimhart90@gmail.com>
2016-11-21 15:23:20 -0500

    form validation + error/thankyou message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -42,12 +42,36 @@ const experiment = () => {
   let button = document.getElementById('submit-btn');
 
   // Let's say we typically make our buttons green, and we want to do some A/B testing to see if a different color encourages more clicks (and more email signups). The control group will get green as usual, and the variation group will get a red button.
-
   if (userGroup === 'variation') {
     button.style.background = '#BF0012';
   } else {
     button.style.background = '#50A13E';
   };
 };
+
+
+
+// Form validation (without actually posting anywhere, allows the page to be static and operate without a db of users.) I strayed away from jQuery for the rest of the assignment, but found it was much easier to use here, alongside the Bootstrap validation plugin.
+
+let $message = $('.form-message');
+let emailList = [];
+
+const submitForm = () => {
+  let newEmail = $('#email').val();
+  if ($.inArray(newEmail, emailList) !== -1) {
+    $message.html('<p class="error">That email is already in our system!</p>');
+  } else {
+    emailList.push(newEmail);
+    $message.html('<p class="thank-you">Thanks for signing up!</p>');
+  };
+
+  $('#email-form').each(function() {
+    this.reset();
+  });
+};
+
+
+
+
 
 
